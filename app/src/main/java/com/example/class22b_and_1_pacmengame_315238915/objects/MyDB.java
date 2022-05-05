aa7ef0fd92f923e3c833ec9f3dd1c57a00964dba
lironshish <lironshish1@gmail.con>
2022-05-05 17:15:03 +0300

    DB

---
+++ app/src/main/java/com/example/class22b_and_1_pacmengame_315238915/objects/MyDB.java
@@ -0,0 +1,39 @@
+package com.example.class22b_and_1_pacmengame_315238915.objects;
+
+import java.util.ArrayList;
+
+public class MyDB {
+    private ArrayList<Record> records;
+    private static MyDB myDB;
+
+    public MyDB() {
+        this.records =  new ArrayList<>();
+    }
+
+    public static MyDB initMyDB() {
+        if (myDB == null) {
+            myDB = new MyDB();
+        }
+        return myDB;
+    }
+
+    public void addRecord (Record record) {
+        records.add(record);
+    }
+
+    public Record getSpecificRecord (int position) {
+        return records.get(position);
+    }
+
+    public ArrayList<Record> getRecords() {
+        return records;
+    }
+
+//    public void  sortByScore () {
+//        Collections.sort(records);
+//    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/class22b_and_1_pacmengame_315238915/objects/Record.java
@@ -0,0 +1,48 @@
+package com.example.class22b_and_1_pacmengame_315238915.objects;
+
+public class Record {
+    private int score = 0;
+    private String name = "";
+    private double lat = 0.0;
+    private double lon = 0.0;
+
+    public Record() {
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public Record setScore(int score) {
+        this.score = score;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Record setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public Record setLat(double lat) {
+        this.lat = lat;
+        return this;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public Record setLon(double lon) {
+        this.lon = lon;
+        return this;
+    }
+
+}
